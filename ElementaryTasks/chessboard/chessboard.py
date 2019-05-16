9fe1eaa4239f16414eb57941d60a5f7fdb82bdbc
AnastasiiaNabok <nastya.nabok@gmail.com>
2019-05-17 00:58:46 +0300

    Update ChessBoard: change validation and unit tests

--- ElementaryTasks/chessboard/chessboard.py
+++ ElementaryTasks/chessboard/chessboard.py
@@ -1,10 +1,18 @@
 class ChessBoard:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.width = self.validate_input(width)
+        self.height = self.validate_input(height)
 
     def __str__(self):
         return self.create_board()
+
+    @staticmethod
+    def validate_input(input_value):
+        if input_value.isdigit() and int(input_value) > 0:
+            val = int(input_value)
+            return val
+        else:
+            raise ValueError('Value "{}" is not correct. Should be positive number'.format(input_value, ))
 
     def create_board(self):
         board = str()
@@ -18,25 +26,23 @@ class ChessBoard:
         return board
 
 
-def validate_input(input_value):
-    try:
-        val = int(input_value)
-        if val > 0:
-            return val
-        else:
-            raise ValueError
-    except ValueError:
-        print("That's not a number!")
-        new_val = validate_input(input('Please enter positive number:'))
-        return int(new_val)
+def try_again():
+    try_again = input('Press "y" or "yes" if you want to try again.  Press any key if you want to stop. ')
+    if try_again.lower() == 'y' or try_again.lower == "yes":
+        main()
 
 
 def main():
-    width = validate_input(input('Please enter board with: '))
-    height = validate_input(input('Please enter board height: '))
+    width = input('Please enter board with: ')
+    height = input('Please enter board height: ')
 
-    board = ChessBoard(width, height)
-    print(board)
+    try:
+        board = ChessBoard(width, height)
+        print(board)
+        try_again()
+    except ValueError as e:
+        print(e)
+        try_again()
 
 
 if __name__ == '__main__':
--- ElementaryTasks/chessboard/test_chessboard.py
+++ ElementaryTasks/chessboard/test_chessboard.py
@@ -6,9 +6,29 @@ from chessboard import ChessBoard
 class TestChessBoard(unittest.TestCase):
 
     def test_create_board(self):
-        chessboard1 = ChessBoard(5, 3)
+        width = '5'
+        height = '3'
+        chessboard1 = ChessBoard(width, height)
         self.assertEqual(chessboard1.create_board(), "* * *\n * * \n* * *\n")
         self.assertNotEqual(chessboard1.create_board(), "* * * *")
+
+    def test_negative_validation(self):
+        width = '-2'
+        height = '10'
+        with self.assertRaises(ValueError):
+            ChessBoard(width, height)
+
+    def test_text_validation(self):
+        width = '10'
+        height = "test"
+        with self.assertRaises(ValueError):
+            ChessBoard(width, height)
+
+    def test_zero_validation(self):
+        width = '0'
+        height = '10'
+        with self.assertRaises(ValueError):
+            ChessBoard(width, height)
 
 
 if __name__ == '__main__':
