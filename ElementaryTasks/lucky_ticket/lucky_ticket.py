6dd22551be11c26318742d76a5be3c4fa5c239c4
AnastasiiaNabok <nastya.nabok@gmail.com>
2019-05-24 12:35:07 +0300

    Update Lucky Ticket

--- ElementaryTasks/lucky_ticket/lucky_ticket.py
+++ ElementaryTasks/lucky_ticket/lucky_ticket.py
@@ -4,6 +4,9 @@ import os
 class LuckyTicket:
     def __init__(self, filename):
         self.filename = filename
+        self.__validate_path()
+
+    def __validate_path(self):
         if os.path.exists(self.filename):
             with open(self.filename, 'r', encoding='utf-8') as f:
                 lines = f.read().splitlines()
@@ -16,8 +19,8 @@ class LuckyTicket:
         count = 0
         for ticket in self.tickets_list:
             ticket_half_len = len(ticket) // 2
-            first_part = sum(map(lambda x: int(x), ticket[:ticket_half_len]))
-            second_part = sum(map(lambda x: int(x), ticket[ticket_half_len:]))
+            first_part = sum(map(int, ticket[:ticket_half_len]))
+            second_part = sum(map(int, ticket[ticket_half_len:]))
             if first_part == second_part:
                 count += 1
         return count
@@ -25,8 +28,8 @@ class LuckyTicket:
     def piter(self):
         count = 0
         for ticket in self.tickets_list:
-            first_part = sum(map(lambda x: int(x), ticket[0::2]))
-            second_part = sum(map(lambda x: int(x), ticket[1::2]))
+            first_part = sum(map(int, ticket[0::2]))
+            second_part = sum(map(int, ticket[1::2]))
             if first_part == second_part:
                 count += 1
         return count
@@ -37,7 +40,7 @@ class LuckyTicket:
         elif self.mode.lower() == 'piter':
             count = self.piter()
         else:
-            raise ValueError('Value {} is not correct mode'.format('mode'))
+            raise ValueError(f'Value {self.mode} is not correct mode')
         return self.mode, count
 
 
@@ -51,7 +54,7 @@ def main():
     file_path = input('Enter filename: ')
     try:
         mode, count = LuckyTicket(file_path).verify_ticket()
-        print('We have {} "{}" lucky ticket(s) in the file {}'.format(count, mode, file_path ))
+        print(f'We have {count} "{mode}" lucky ticket(s) in the file {file_path}')
         try_again()
     except (ValueError, FileExistsError) as e :
         print(e)
--- ElementaryTasks/lucky_ticket/test_lucky_ticket.py
+++ ElementaryTasks/lucky_ticket/test_lucky_ticket.py
@@ -1,5 +1,32 @@
 import unittest
 
+from lucky_ticket import LuckyTicket
+
+
+class TestLuckyTicket(unittest.TestCase):
+
+    def test_file_path_failure(self):
+        file_path = 'test.txt'
+        with self.assertRaises(FileExistsError):
+            LuckyTicket(file_path)
+
+    def test_moscow_ticket(self):
+        file_path = 'test_moscow_tickets.txt'
+        expected_count = 2
+        actual_count = LuckyTicket(file_path).moscow()
+        self.assertEqual(actual_count, expected_count)
+
+    def test_piter_ticket(self):
+        file_path = 'test_piter_tickets.txt'
+        expected_count = 2
+        actual_count = LuckyTicket(file_path).piter()
+        self.assertEqual(actual_count, expected_count)
+
+    def test_not_correct_mode(self):
+        file_path = 'test_tickets.txt'
+        with self.assertRaises(ValueError):
+            LuckyTicket(file_path).verify_ticket()
+
 
 if __name__ == '__main__':
     unittest.main()
